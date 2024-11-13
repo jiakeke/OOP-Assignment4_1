0227e723a9eda838ec5f33dbaee3d0569762c283
Qingyun Wang <lucas.qingyun@gmail.com>
2024-11-14 00:13:53 +0200

    simplify main and checkPalindrome method

--- Palindrome/src/PalindromeChecker.java
+++ Palindrome/src/PalindromeChecker.java
@@ -1,14 +1,14 @@
+import java.util.Scanner;
+
 public class PalindromeChecker {
 
     public static void main(String[] args) {
+        System.out.println("Enter a string to check if it is a palindrome:");
 
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
 
-        String transformed = transformInput(input);
-
-        boolean isPalindrome = checkPalindrome(transformed);
-        if (isPalindrome) {
+        if (checkPalindrome(transformInput(input))) {
             System.out.println("The transformed input is a palindrome.");
         } else {
             System.out.println("The transformed input is not a palindrome.");
@@ -16,25 +16,20 @@ public class PalindromeChecker {
     }
 
     public static String transformInput(String input) {
-        input = input.toLowerCase();
-        input = input.replace(",", "");
-        input = input.replace("!", "");
+        input = input.toLowerCase(); // Convert to lowercase
+        input = input.replaceAll("\\p{Punct}", ""); // Remove punctuation
         return input;
     }
 
     public static boolean checkPalindrome(String input) {
         String reversed = reverseString(input);
-        if (input.equals(reversed)) {
-            return true;
-        } else {
-            return false;
-        }
+        return input.equals(reversed);
     }
 
     public static String reverseString(String input) {
         StringBuilder reversed = new StringBuilder();
         for (int i = input.length() - 1; i >= 0; i--) {
-            reversed.append(input.charAt(i));
+            reversed.append(input.charAt(i)); // Append each character in reverse order
         }
         return reversed.toString();
     }
